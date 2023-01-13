eb4d17c7bf5c7cc65a32be992c2e2c9b2d5c2896
Gabriel Lourenco <lourencogabriel77@gmail.com>
2023-01-13 10:22:58 -0300

    modulo back-end finalizado

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -21,13 +21,15 @@ class CategoryController {
         }
 
         const { name } = req.body;
+        const { filename: path } = req.file;
+
         const categoryExist = await Category.findOne({ where: { name } });
 
         if (categoryExist) {
             return res.status(400).json({ error: "Category already exists" });
         }
 
-        const category = await Category.create({ name })
+        const category = await Category.create({ name, path })
 
         return res.json({ name, id: category.id });
     }
@@ -36,6 +38,41 @@ class CategoryController {
         const category = await Category.findAll();
         return res.json(category);
     }
+
+    async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string()
+        });
+
+        try {
+            await schema.validateSync(req.body, { abortEarly: false });
+        } catch (err) {
+            return res.status(400).json({ error: err.errors });
+        }
+
+        const { admin: idAdmin } = await User.findByPk(req.userId);
+
+        if (!idAdmin) {
+            res.status(401).json()
+        }
+
+        const { name } = req.body;
+        const { id } = req.params;
+        const category = await Category.findByPk(id);
+        if (!category) {
+            return res.status(404).json({ error: "make sure your category id id correct" });
+        }
+
+        let path;
+        if (req.file) {
+            path = req.file.filename;
+        }
+
+        await Category.update({ name, path }, { where: { id } })
+
+        return res.status(200).json();
+    }
+
 }
 
 export default new CategoryController();
